3f960edfac81b37528143e9a4c82cd7ea0819ff1
Edward Zhou <edwardzhou@EdwarddeMacBook-Pro.local>
2020-01-05 16:16:40 +0800

    upgrade ecto to 3.x

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TimexEcto.Mixfile do
   def project do
     [
       app: :timex_ecto,
-      version: "3.4.0",
+      version: "3.4.1",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
@@ -32,7 +32,7 @@ defmodule TimexEcto.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [{:timex, "~> 3.6"},
-     {:ecto, "~> 2.2"},
+     {:ecto, "~> 3.0"},
      {:postgrex, "~> 0.13", only: :test},
      {:ex_doc, "~> 0.13", only: :dev}]
   end
